79e7c3932a0fb1e8b927e7b2b8ae9f711e360673
William C. Dawn <dawnwc@ornl.gov>
2017-07-24 10:19:03 -0400

    changed == to a soft compare. need to add operator next.

--- salt_1d.f90
+++ salt_1d.f90
@@ -11,6 +11,7 @@ integer :: i,nStep
 real(8) :: alpha,beta,gamma,delta
 real(8) :: h
 real(8) :: temp
+real(8),parameter :: tol = 1.0d-10
 
 tlo = 470.0d0 + 273.15d0 ! [K]
 thi = 650.0d0 + 273.15d0 ! [K]
@@ -21,7 +22,7 @@ do i = 1,nStep
   temp = calc_T(calc_h(T))
   ! need a soft-equals function
   write(*,'(3(e12.6,x),l2)') T, temp, (T - temp), (T == temp)
-  if (T /= temp) then
+  if (abs(T - temp) > tol) then
     write(*,'(a)') 'T /= calc_T(calc_h(T))'
     write(*,'(a,e12.6)') 'T = ',T
     write(*,'(a,e12.6)') 'calc_T(calc_h(T)) = ', temp
@@ -29,6 +30,7 @@ do i = 1,nStep
   endif
   T = T + dt
 enddo
+write(*,'(a)') 'success'
 
 ! open(unit = 11, file = 'h_of_T.out',status = 'replace',action = 'write',iostat = ios)
 ! if (ios /= 0) then
